5d9fa2fced59420ac0dab705dfcfb71f1abdc67f
Joshmitha-2000 <djoshmithadevarakonda@gmail.com>
2025-06-04 14:49:27 +0530

    update apis

--- services/authservice.js
+++ services/authservice.js
@@ -21,7 +21,7 @@ async function registerUser(name, email, password, role) {
 
   // create confirmation token valid for 24 day
   const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '24d' });
-  const confirmUrl = `http://localhost:5000/api/auth/confirm/${token}`;
+  const confirmUrl = `https://ticketbooking-backend-sr3r.onrender.com/api/auth/confirm/${token}`;
 
   await sendConfirmationEmail(email, confirmUrl);
 
